6084eb434c84161830b3957ac464181f5727f1aa
sam21733 <samcomputerucd@gmail.com>
2024-09-14 07:23:11 +0500

    Update comparisons.py

--- comparisons.py
+++ comparisons.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the data
-data = pd.read_csv('/workspaces/student/StudentsPerformance.csv')
+data = pd.read_csv('StudentsPerformance.csv')
 
 df = pd.DataFrame(data)
 
